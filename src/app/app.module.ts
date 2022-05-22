269b839b731dd09a76ec533cb20b27cdb8fb1731
Jona-xn <95232734+Jona-xn@users.noreply.github.com>
2022-05-21 23:53:01 -0300

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HardYSoftSkillsComponent } from './components/hard-y-soft-skills/hard-y-soft-skills.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    BannerComponent,
+    HardYSoftSkillsComponent,
+    LogoAPComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
